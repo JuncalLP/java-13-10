a96c162c0dca3c59e994884d118e0cbc524132c9
JuncalLP <92026660+JuncalLP@users.noreply.github.com>
2021-10-26 23:19:09 +0200

    Java

--- 07-SumaNumeros/src/com/liceolapaz/des/jjv/Principal.java
+++ 07-SumaNumeros/src/com/liceolapaz/des/jjv/Principal.java
@@ -2,46 +2,45 @@ package com.liceolapaz.des.jjv;
 
 import java.util.Scanner;
 
+
 public class Principal {
 
 	public static void main(String[] args) {
-// Pedir números al usuario
+		// Pedir números al usuario
 		pedirNumeros();
-		// Crear variable para almacenar los números
-		int[] numeros = new int[10]; 
+		// Crear variable para almacenar números
+		int[] numeros = new int[10];
 		// Leer números
-		int contador = 0; 
+		int contador = 0;
 		do {
 			numeros[contador] = leerNumero();
 			contador++;
-			
 		} while(contador < 10);
-		// Calcular suma y almacenar en una variable
+		// Calcular suma y almacenarla
 		int sumaTotal = calcularSuma(numeros);
 		// Escribir el resultado
 		escribirResultado(sumaTotal);
 	}
 
 	private static void escribirResultado(int sumaTotal) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("La suma de los números es " + sumaTotal);
 	}
 
 	private static int calcularSuma(int[] numeros) {
 		int total = 0;
-		for (int i = 0 : i < numeros.length; i++) {
-			
-		} 
+		for (int i = 0; i < numeros.length; i++) {
+			total += numeros[i]; // total = total + numeros[i];
+		}
 		return total;
 	}
 
-	private static void leerNumero() {
-Scanner escaner = new Scanner(System.in);
-return escaner.nextInt();
+	private static int leerNumero() {
+		Scanner escaner = new Scanner(System.in);
+		return escaner.nextInt();
 	}
 
 	private static void pedirNumeros() {
-System.out.println("Escriba");		
+		System.out.println("Escriba 10 números enteros:");
 	}
 
 }
